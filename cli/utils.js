f5c075c7fd626b632413e8ab93ebd380138551dc
Pierre Ernst <pierre.ernst@elastic.co>
2020-03-23 15:18:17 -0400

    code style

--- cli/utils.js
+++ cli/utils.js
@@ -1,23 +1,23 @@
 const compareText = (a, b) => {
-  const _a = a.toLowerCase();
-  const _b = b.toLowerCase();
-  if (_a < _b) {
-    return -1;
-  } else if (_a > _b) {
-    return 1;
-  }
-  return 0;
+    const _a = a.toLowerCase();
+    const _b = b.toLowerCase();
+    if (_a < _b) {
+        return -1;
+    } else if (_a > _b) {
+        return 1;
+    }
+    return 0;
 };
 
 const capitalize = (s) => {
-  if (typeof s !== 'string') return ''
-  return s.charAt(0).toUpperCase() + s.slice(1)
-}
+    if (typeof s !== 'string') return '';
+    return s.charAt(0).toUpperCase() + s.slice(1);
+};
 
 const uniq = (array) => [...new Set(array)];
 
 module.exports = {
-  capitalize,
-  compareText,
-  uniq
+    capitalize,
+    compareText,
+    uniq,
 };
